a9afd1d28ccd02773fcfd841c4df71d641617239
jdfabs <jdfabs1@gmail.com>
2024-10-27 17:28:30 +0000

    git ignore update

---
+++ src/client/sudoku_solver_utils.c
@@ -0,0 +1,28 @@
+//
+// Created by skipper on 25-10-2024.
+//
+
+#include "common.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "client.h"
+
+void solve_by_brute_force(int matrix[][BOARD_SIZE], int solution[][BOARD_SIZE]) {
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (matrix[i][j] == 0) {
+				printf("celula (%d,%d) está vazia\n", i, j);
+				for (int k = 1; k <= BOARD_SIZE; k++) {
+					printf("a tentar %d\n", k);
+					if (k == solution[i][j]) {
+						matrix[i][j] = k;
+						printf("Numero correto encontrado\n");
+						printBoard(matrix);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
